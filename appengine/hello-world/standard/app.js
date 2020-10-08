74c801e20d810d65a9aeb5065bb2ea7066b3cea7
sathishzeo <45935603+sathishzeo@users.noreply.github.com>
2020-10-08 14:49:34 +0530

    Update app.js

--- appengine/hello-world/standard/app.js
+++ appengine/hello-world/standard/app.js
@@ -1,34 +1,18 @@
-// Copyright 2017 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
-'use strict';
+var http = require('http');
+var fs = require('fs');
 
-// [START gae_node_request_example]
-const express = require('express');
+function onRequest(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', null, function(error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+}
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.status(200).render('./index.html').end();
-});
-
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
-// [END gae_node_request_example]
-
-module.exports = app;
+http.createServer(onRequest).listen(8000);
